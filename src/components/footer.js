64a86d9deceecb122f774ab07627af171a72d2f0
Moyinoluwa Adenuga <dayoh14@gmail.com>
2021-08-04 23:05:41 -0700

    added sm icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,8 +3,15 @@ import React from 'react'
 const Footer = () => {
     return(
         <footer>
-            <div>Contact us</div>
             <div>
+                Contact us
+                <span>
+                    <i className='fa fa-twitter' />
+                    <i className='fa fa-youtube-play' />
+                    <i className='fa fa-facebook' />
+                </span>
+            </div>
+            <div className='copyright'>
                 <span>Terms &amp; Conditions. Privacy Policy</span>
                 <span>&copy; Copyright 2021 FoodBag is a registered trademark</span>
             </div>
